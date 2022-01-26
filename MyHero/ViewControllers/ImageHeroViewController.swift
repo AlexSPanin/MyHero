190a0a7d758a4769140699e27ba34188cce93bb5
Panin <panin1970@gmail.com>
2022-01-26 17:34:23 +0300

    add view controller and outlet

---
+++ MyHero/ViewControllers/ImageHeroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageHeroViewController.swift
+//  MyHero
+//
+//  Created by Александр Панин on 26.01.2022.
+//
+
+import UIKit
+
+class ImageHeroViewController: UIViewController {
+    @IBOutlet var imageHeroView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
